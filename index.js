aed357d4659cbca14d2c93494c121136e522ae8e
mecha2k <mecha2k@naver.com>
2020-05-06 10:46:52 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const app = require("./app")
+const dotenv = require("dotenv")
+const fs = require("fs")
+
+dotenv.config()
+
+// app.get("/", function (req, res) {
+//   // res.send("hello world")
+//   res.status(200).send("Hello from the server side!")
+// })
+//
+// app.post("/", function (req, res) {
+//   res.send("Post method~")
+// })
+
+const tours = JSON.parse(fs.readFileSync("./resources/data/tours-simple.json"))
+
+app.get("/api/tours", function (req, res) {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: { tours },
+  })
+})
+
+app.post("/api/tours", function (req, res) {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+
+  console.log(req.body)
+  tours.push(newTour)
+  fs.writeFile("./resources/data/tours-simple.json", JSON.stringify(tours), (error) => {
+    res.status(201).json({ status: "success", data: { tour: newTour } })
+  })
+})
+
+const port = process.env.PORT || "3000"
+app.listen(port, function () {
+  console.log("Server App running on port: " + port)
+})
