e63c72494ef04780e60e9665058b118c13c9aa24
mecha2k <mecha2k@naver.com>
2020-05-07 21:52:10 +0900

    2nd commit (add mongodb)

--- README.md
+++ README.md
@@ -3,9 +3,15 @@
 Built using modern technologies: node.js, express, mongoDB,
 mongoose and friends 😁
 
+##### Node.js & Express
+* Express : fully wraped node.js server
+
+
 ##### MongoDB connection 
 * Atlas Database : DB
 * Local Database : localDB
+* Changing database name from 'test' to 'natours'
+* Password is saved in '.env' file
 
 ~~~
   const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
--- index.js
+++ index.js
@@ -8,10 +8,31 @@ dotenv.config({ path: "./.env" })
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
 const localDB = process.env.DATABASE_LOCAL
 mongoose
-  .connect(localDB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+  .connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
   .then((connect) => {
     console.log(connect.connection)
     console.log("Database connection successful.")
+  })
+
+const tourSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "A tour must have a name"], unique: true },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, "A tour must have a price"] }
+})
+const Tour = mongoose.model("Tour", tourSchema)
+
+const testTour = new Tour({
+  name: "The Park Camper",
+  price: 977
+})
+
+testTour
+  .save()
+  .then(function(doc) {
+    console.log(doc)
+  })
+  .catch(function(err) {
+    console.log("ERROR :", err)
   })
 
 // app.get("/", function (req, res) {
