49fb59f12ae3d0d514a03070a572bffb2e8ae5bf
mecha2k <mecha2k@naver.com>
2020-05-09 23:12:03 +0900

    express restruct files...

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const app = express()
 //
 app.use(logger("dev"))
 app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
-// app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use(function(req, res, next) {
