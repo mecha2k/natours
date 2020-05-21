deb17f8c1c5ee009b9903962732c5cadabfff109
mecha2k <mecha2k@naver.com>
2020-05-21 14:04:08 +0900

    Merge branch 'master' of https://github.com/mecha2k/natours
     Conflicts:
    	README.md
    	index.js
    	resources/data/tours-simple.json

--- models/tours.js
+++ models/tours.js
@@ -2,60 +2,136 @@ const mongoose = require("mongoose")
 const slugify = require("slugify")
 const validator = require("validator")
 
-const shema = new mongoose.Schema(
+const schema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: [true, "A tour must have a name"],
       unique: true,
       trim: true,
-      minlength: [10, "A tour name must have more or equal than 10 characters"],
-      maxlength: [40, "A tour name must have less or equal than 40 characters"],
-      validate: [validator.isAlpha, "A tour name must only contain characters"],
+      maxlength: [40, "A tour name must have less or equal then 40 characters"],
+      minlength: [10, "A tour name must have more or equal then 10 characters"],
+      validate: [validator["isAlpha"], "Tour name must only contain characters"]
     },
-    duration: { type: Number, required: [true, "A tour must have a duration"] },
-    maxGroupSize: { type: Number, required: [true, "A tour must have a group size"] },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"]
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a group size"]
+    },
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
       enum: {
         values: ["easy", "medium", "difficult"],
-        message: "Difficulty is either: easy, medium, difficult",
-      },
+        message: "Difficulty is either: easy, medium, difficult"
+      }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
       min: [1, "Rating must be above 1.0"],
       max: [5, "Rating must be below 5.0"],
+      set: function (val) {
+        return Math.round(val * 10) / 10
+      }
     },
-    ratingsQuantity: { type: Number, default: 0 },
-    slug: String,
-    price: { type: Number, required: [true, "A tour must have a price"] },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"]
+    },
     priceDiscount: {
       type: Number,
-      validate: function (value) {
-        return value < this.price
-      },
-      message: "Discount price ({VALUE}) should be below regular one",
+      validate: {
+        validator: function (val) {
+          return val < this["price"]
+        },
+        message: "Discount price ({VALUE}) should be below regular price"
+      }
     },
-    summary: { type: String, trim: true, required: [true, "A tour must have a summary"] },
-    description: { type: String, trim: true },
-    imageCover: { type: String, required: [true, "A tour must have a cover image"] },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description"]
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image"]
+    },
     images: [String],
-    createdAt: { type: Date, default: Date.now(), select: false },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
     startDates: [Date],
-    secretTour: { type: Boolean, default: false },
+    secretTour: {
+      type: Boolean,
+      default: false
+    },
+    startLocation: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"]
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: ["Point"]
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User"
+      }
+    ]
   },
-  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 )
 
-shema.virtual("durationWeeks").get(function () {
+schema.index({ price: 1, ratingsAverage: -1 })
+schema.index({ slug: 1 })
+schema.index({ startLocation: "2dsphere" })
+
+schema.virtual("durationWeeks").get(function () {
   return this.duration / 7
 })
 
+schema.virtual("reviews", {
+  ref: "Review",
+  foreignField: "tour",
+  localField: "_id"
+})
+
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
-shema.pre("save", function (next) {
+schema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true })
   next()
 })
@@ -70,26 +146,25 @@ shema.pre("save", function (next) {
 //   next();
 // });
 
-// QUERY MIDDLEWARE: shema.pre('find', function(next) {
-shema.pre(/^find/, function (next) {
+schema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } })
 
   this.start = Date.now()
   next()
 })
 
-shema.post(/^find/, function (docs, next) {
+schema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`)
   next()
 })
 
 // AGGREGATION MIDDLEWARE
-shema.pre("aggregate", function (next) {
+schema.pre("aggregate", function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
   console.log(this.pipeline())
   next()
 })
 
-const Tour = mongoose.model("Tour", shema)
+const Tour = mongoose.model("Tour", schema)
 
 module.exports = Tour
