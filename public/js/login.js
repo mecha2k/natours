4dce1575c09007d15ec1098b31a9e8058f54801a
mecha2k <mecha2k@naver.com>
2020-06-21 02:57:57 +0900

    express restruct files...

--- public/js/login.js
+++ public/js/login.js
@@ -1,49 +1,46 @@
 // const axios = require("axios")
-import axios from "axios"
-import { showAlert } from "./alerts"
+// import axios from "axios"
+// import { showAlert } from "./alerts"
 
-export const login = async function(email, password) {
+const login = async function(email, password) {
   try {
     const res = await axios({
       method: "POST",
-      url: "/api/users/login",
+      url: "http://localhost:3000/api/users/login",
       data: {
         email,
         password
       }
     })
+    // .then((res) => console.log(res))
+    // .catch((err) => console.log(err))
 
-    // if (res.data.status === "success") {
-    //   showAlert("success", "Logged in successfully!")
-    //   window.setTimeout(function () {
-    //     location.assign("/")
-    //   }, 1500)
-    // }
+    if (res.data.status === "success") {
+      alert("Logged in successfully!")
+      window.setTimeout(function() {
+        location.assign("/")
+      }, 100)
+    }
     console.log(res)
   } catch (err) {
-    // showAlert("error", err.response.data.message)
-    console.log(err.response.message)
+    alert(err.response.data.message)
+    console.log(err.response.data.message)
+    // message : Incorrect email or password (need to correct!)
   }
 }
-//
-// export const logout = async function () {
-//   try {
-//     const res = await axios({
-//       method: "GET",
-//       url: "/api/v1/users/logout"
-//     })
-//     if ((res.data.status = "success")) location.reload(true)
-//   } catch (err) {
-//     console.log(err.response)
-//     showAlert("error", "Error logging out! Try again.")
-//   }
-// }
 
-// const login = function (email, password) {
-//   alert({email, password})
-//   console.log(email)
-//   console.log(password)
-// }
+const logout = async function() {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: "/api/v1/users/logout"
+    })
+    if ((res.data.status = "success")) location.reload(true)
+  } catch (err) {
+    console.log(err.response)
+    alert("Error logging out! Try again.")
+  }
+}
 
 const loginForm = document.querySelector(".form")
 
@@ -52,5 +49,7 @@ if (loginForm)
     e.preventDefault()
     const email = document.getElementById("email").value
     const password = document.getElementById("password").value
-    login(email, password).then(console.log({ email, password }))
+    login(email, password)
+      .then(console.log({ email, password }))
+      .catch((err) => console.log(err))
   })
